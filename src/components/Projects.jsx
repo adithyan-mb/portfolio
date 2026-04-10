bed7a4c32d1ef9e6449f2be0676274d5fd337519
ADITHYAN M B <84918745+adithyan-mb@users.noreply.github.com>
2026-04-10 16:22:58 +0000

    updated projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,12 +3,14 @@ import Website from '../images/Website.svg?react'
 import Card from '../components/ReusableCard.jsx'
 import amazon from '../images/projeectIcons/amazon.png'
 import weather from '../images/projeectIcons/weatherApp.png'
-import tictactoe from'../images/projeectIcons/tic_tac_toe.png'
-import todo from '../images/projeectIcons/todo.png'
+import tictactoe from'../images/projeectIcons/cli_tic_tac_toe.png'
+// import todo from '../images/projeectIcons/todo.png'
+import home_automation from '../images/projeectIcons/automated-home.png'
 import  portfolio from '../images/projeectIcons/portfolio.png'
-import notfound from '../images/projeectIcons/notfound.png'
-import movieinfo from  '../images/projeectIcons/movieinfo.png'
-import address_book from '../images/projeectIcons/address_book.jpg'
+import lex from  '../images/projeectIcons/lexical.png'
+//import notfound from '../images/projeectIcons/notfound.png'
+// import movieinfo from  '../images/projeectIcons/movieinfo.png'
+// import address_book from '../images/projeectIcons/address_book.jpg'
 import lsb from '../images/projeectIcons/lsb_image_steganography.png'
 import mp3 from '../images/projeectIcons/mp3_tag_reader.jpg'
 
@@ -19,17 +21,26 @@ const Projects = () => {
       
       <div className='pt-3'>
          <div className='grid grid-cols-3 p-2 grid-rows-2 items-center gap-y-7 justify-items-center w-3/5'> 
-           <Card 
-           src={todo} 
-           title={"Home automation" }  
-           description={"Home automation monitoring through MQTT."} 
-           tech={"C++ · MQTT protocol  Wi-Fi / local network"}
-           handleClick={()=>{window.open("","blank")}}
+           <Card src={tictactoe}
+            title={"Multiplayer Tic Tac Toe game over LAN" } 
+           description={"Two-player Tic Tac Toe game through CLI."} 
+           tech={"Python · Python socket · Wi-Fi / local network· CLI "}
+           handleClick={()=>{window.open("https://github.com/adithyan-mb/Tic-Tac-toe-with-sockets","blank")}}
            />
-     <Card src={movieinfo} 
-           title={"State monitor" } 
-           description={"Industrial Machine State Monitoring System  of a conveyor belt."} 
-           tech={"C++ · ESP32 · Wokwi simulator · ThingSpeak"}
+
+           <Card 
+           src={home_automation} 
+           title={"Home automation using HTTP" }  
+           description={"Home automation monitoring through MQTT."} 
+           tech={"HTTP protocol · Python flask · Arduino IDE · ESP32 · Wi-Fi / local network "}
+           handleClick={()=>{window.open("https://github.com/adithyan-mb/Home-automation-using-HTTP","blank")}}
+           />
+
+          <Card src={lex} 
+           title={"Lexical Analyzer" } 
+           description={"Simple Lexical Analyzer in C "} 
+           tech={"C · File operations · Tokenization "}
+           handleClick={()=>{window.open("https://github.com/adithyan-mb/lexical-analyzer","blank")}}
            />
          
 
@@ -47,12 +58,7 @@ const Projects = () => {
              handleClick={()=>{window.open("https://github.com/adithyan-mb/lsb_image_steganography","blank")}}
              />
 
-               <Card src={address_book}
-            title={"Address book" } 
-           description={"Digital adaptation of the physical Address book."} 
-           tech={"C · Structures · Functions · File Handling"}
-           handleClick={()=>{window.open("https://github.com/adithyan-mb/address_book","blank")}}
-           />
+             
           
            <Card src={portfolio} 
            title={"Portfolio" } 
